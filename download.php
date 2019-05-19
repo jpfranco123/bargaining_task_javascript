167e47699771ad07a0937100c0404a31a98d81b4
“jpfranco123” <jpfranco123@gmail.com>
2019-05-19 14:38:24 +1000

    download.php

--- download.php
+++ download.php
@@ -3,19 +3,29 @@ include "commonSlider.inc";
 
 $folder=$_POST["folder"];
 
+downloadCSVSQL("commonParameters",$folder,$session);
+downloadCSVSQL("matching",$folder,$session);
+downloadCSVSQL("paymentSession",$folder,$session);
+downloadCSVSQL("sliderLog",$folder,$session);
+downloadCSVSQL("subjects",$folder,$session);
+downloadCSVSQL("trialInfo",$folder,$session);
 
-downloadCSV("commonParameters",$folder,$session);
-downloadCSV("LogChats",$folder,$session);
-downloadCSV("matching",$folder,$session);
-downloadCSV("paymentSession",$folder,$session);
-downloadCSV("sliderLog",$folder,$session);
-downloadCSV("subjects",$folder,$session);
-downloadCSV("trialInfo",$folder,$session);
+downloadCSVSQL("socialPref",$folder,$session);
+downloadCSVSQL("matchingSP",$folder,$session);
 
-downloadCSV("socialPref",$folder,$session);
-downloadCSV("matchingSP",$folder,$session);
+downloadCSVSQL("timeMarks",$folder,$session);
 
-downloadCSV("timeMarks",$folder,$session);
+// downloadCSV("commonParameters",$folder,$session);
+// downloadCSV("matching",$folder,$session);
+// downloadCSV("paymentSession",$folder,$session);
+// downloadCSV("sliderLog",$folder,$session);
+// downloadCSV("subjects",$folder,$session);
+// downloadCSV("trialInfo",$folder,$session);
+//
+// downloadCSV("socialPref",$folder,$session);
+// downloadCSV("matchingSP",$folder,$session);
+//
+// downloadCSV("timeMarks",$folder,$session);
 
 
 //downloadCSV1("commonParameters",$folder,$session);
