abed406e1a5845304a4982054f6927e18cc7de1b
Pablo Franco <jpfranco123@gmail.com>
2019-09-22 14:40:23 +1000

    Instructions 8 added and Instructions 6 modified.

---
+++ instructions8.php
@@ -0,0 +1,49 @@
+<?php
+include("commonSlider.inc");
+Header("Cache-Control: must-revalidate");
+
+if (!$_COOKIE['theCookie']){
+  header("Location: begin.html");
+  exit();
+}
+
+$koek=readcookie("theCookie");
+$ppnr=$koek[0];
+updateTableOne("subjects","ppnr=$ppnr","currentpage",$_SERVER['PHP_SELF']);
+$part=1;
+$menu=instructionMenu($_SERVER['PHP_SELF'], $ppnr,$part);
+
+$insNextPage=  instructionsNextPage($_SERVER['PHP_SELF'], $ppnr, $part);
+
+$insPrevPage=  instructionsPrevPage($_SERVER['PHP_SELF'], $ppnr, $part);
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+  <TITLE> Instructions 8 </TITLE>
+  <link rel="stylesheet" type="text/css" href="beleggensns.css" />
+  <link rel="stylesheet" type="text/css" href="buttons.css" />
+  <link rel="stylesheet" href="generalConfigInstruct.css">
+
+</HEAD>
+
+<BODY>
+  <table width="60%" border="0" cellpadding="0" cellspacing="0" align="center">
+    <tr><td>
+    <p align=center><?php echo $menu; ?></p>
+    <H1 align=center> Final Payment Calculation</H1>
+
+    <p> At the end of the experiment <?php echo $totalPayTrials; ?> random rounds will be selected.
+
+    <p> You will be paid the total amount of money that you earned in those rounds.</p>
+
+    <br>
+    <p style="float:right"><a href=<?php echo $insNextPage; ?> class="buttonblauw">Continue</a>
+    <br>
+    <p align=left><a href=<?php echo $insPrevPage; ?> class="buttonblauw">Back</a>
+    </td></tr>
+  </table>
+
+</BODY>
+</HTML>
